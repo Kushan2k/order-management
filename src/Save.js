9dac40006d4a71dc155c41abd8d1b150c8e30f61
Kushan2k <kushangayantha001@gmail.com>
2023-11-29 14:29:25 +0530

    blob to pdf

--- src/Save.js
+++ src/Save.js
@@ -31,6 +31,7 @@ function Save() {
       >
         Back
       </button>
+
       <div className="row">
         <p className="text-center">Genarating please wait..</p>
         <div className="col-6 mx-auto d-flex justify-content-center align-items-center">
@@ -40,17 +41,23 @@ function Save() {
             <PDFDownloadLink
               // onClick={() => navigation("/")}
               document={<PDF name={name} items={items} loading={setloading} />}
-              fileName={`${name}-${new Date().toISOString()}`}
+              fileName={`${name}-${new Date().toISOString()}.pdf`}
               style={{ borderRadius: "50%", height: 70, width: 70 }}
               className="btn btn-success p-3 d-flex justify-content-center align-items-center"
             >
-              {({ blob, url, loading, error }) =>
-                loading ? (
+              {({ blob, url, loading, error }) => {
+                const b = URL.createObjectURL(blob)
+                return (
+                  <a href={b} download>
+                    download
+                  </a>
+                )
+                return loading ? (
                   <Spinner variant="light" animation="border" />
                 ) : (
                   "Save"
                 )
-              }
+              }}
             </PDFDownloadLink>
           )}
         </div>
